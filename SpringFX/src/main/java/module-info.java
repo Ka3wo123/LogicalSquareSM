7ee7f556aea3628870ef8cc0772f6ef91a16e667
Kajetan Nowak <kajtek2n@gmail.com>
2023-11-03 17:28:17 +0100

    Removed SpringFX module - unnecessary

--- SpringFX/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pl.logicalsquare.springfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens pl.logicalsquare.springfx to javafx.fxml;
-    exports pl.logicalsquare.springfx;
-}
